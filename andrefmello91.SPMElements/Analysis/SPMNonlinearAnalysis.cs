7d64d543eb9b93f9c5635a8fd59287446853c741
andremello91 <andremello91@gmail.com>
2021-05-11 20:21:54 -0400

    Added nonlinear SPM analysis class

---
+++ andrefmello91.SPMElements/Analysis/SPMNonlinearAnalysis.cs
@@ -0,0 +1,41 @@
+﻿using andrefmello91.FEMAnalysis;
+
+namespace andrefmello91.SPMElements
+{
+	/// <summary>
+	///		Nonlinear analysis class for SPM.
+	/// </summary>
+	public class SPMNonlinearAnalysis : NonlinearAnalysis
+	{
+		/// <inheritdoc />
+		/// <param name="spmInput">The <see cref="SPMInput"/>.</param>
+		public SPMNonlinearAnalysis(SPMInput spmInput, NonLinearSolver solver = NonLinearSolver.NewtonRaphson, int numLoadSteps = 50, double tolerance = 1E-06, int maxIterations = 10000, int minIterations = 2)
+			: base(spmInput, solver, numLoadSteps, tolerance, maxIterations, minIterations)
+		{
+		}
+
+		/// <inheritdoc />
+		protected override void ClearIterations()
+		{
+			base.ClearIterations();
+
+			if (Solver is NonLinearSolver.Secant)
+				return;
+			
+			foreach (var element in FemInput)
+				switch (element)
+				{
+					case NLStringer stringer:
+						stringer.ClearIterations();
+						return;
+
+					case NLPanel panel:
+						panel.ClearIterations();
+						return;
+
+					default:
+						return;
+				}
+		}
+	}
+}

92366443c717d26c92e540d9d96680ca9cfc59ed
andremello91 <andremello91@gmail.com>
2021-08-21 17:09:47 -0400

    Added IsInside method

--- andrefmello91.SPMElements/Extensions.cs
+++ andrefmello91.SPMElements/Extensions.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using andrefmello91.Extensions;
 using andrefmello91.FEMAnalysis;
 using andrefmello91.OnPlaneComponents;
 using andrefmello91.SPMElements.PanelProperties;
+using UnitsNet;
 
 namespace andrefmello91.SPMElements
 {
@@ -64,6 +66,31 @@ namespace andrefmello91.SPMElements
 						break;
 				}
 		}
+
+		/// <summary>
+		///		Check if a point is inside a panel geometry.
+		/// </summary>
+		/// <param name="point">The point.</param>
+		/// <param name="panelGeometry">The panel geometry.</param>
+		/// <returns>
+		///		<b>True</b> if <paramref name="point"/> is inside <paramref name="panelGeometry"/>.
+		/// </returns>
+		public static bool IsInside(this Point point, PanelGeometry panelGeometry) =>
+			point.X.IsBetween(panelGeometry.Edge4.CenterPoint.X, panelGeometry.Edge2.CenterPoint.X) &&
+			point.Y.IsBetween(panelGeometry.Edge1.CenterPoint.Y, panelGeometry.Edge3.CenterPoint.Y);
+
+		/// <summary>
+		///		Check if a point is inside a panel vertices.
+		/// </summary>
+		/// <param name="point">The point.</param>
+		/// <param name="vertices">The vertices.</param>
+		/// <returns>
+		///		<b>True</b> if <paramref name="point"/> is inside <paramref name="vertices"/>.
+		/// </returns>
+		public static bool IsInside(this Point point, Vertices vertices) =>
+			point.X.IsBetween(vertices.Vertex1.MidPoint(vertices.Vertex4).X, vertices.Vertex2.MidPoint(vertices.Vertex3).X) &&
+			point.Y.IsBetween(vertices.Vertex1.MidPoint(vertices.Vertex2).Y, vertices.Vertex4.MidPoint(vertices.Vertex3).Y);
+
 
 		#endregion
 

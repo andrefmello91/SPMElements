abd77b879e8653cdc0fe6901f1e877be1535ae93
andremello91 <andremello91@gmail.com>
2021-08-28 15:59:37 -0400

    Added specific SPM analysis classes

---
+++ andrefmello91.SPMElements/Analysis/SPMAnalysis.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using andrefmello91.FEMAnalysis;
+
+namespace andrefmello91.SPMElements
+{
+	/// <summary>
+	///		Nonlinear analysis class for SPM.
+	/// </summary>
+	public class SPMAnalysis : NonlinearAnalysis
+	{
+		// Load steps of cracking
+		private int? _stringerCrackLS, _panelCrackLS;
+		
+		/// <inheritdoc />
+		public SPMAnalysis(IFEMInput nonlinearInput)
+			: base(nonlinearInput)
+		{
+		}
+
+		/// <inheritdoc />
+		public SPMAnalysis(IFEMInput nonlinearInput, AnalysisParameters parameters)
+			: base(nonlinearInput, parameters)
+		{
+		}
+
+		/// <inheritdoc />
+		protected override void SetStepResults(int? monitoredIndex)
+		{
+			base.SetStepResults(monitoredIndex);
+			
+			if (FemInput is not SPMInput spmInput)
+				return;
+			
+			// Check if a stringer cracked at the current step
+			if (!_stringerCrackLS.HasValue && spmInput.Stringers.Any(s => s is NLStringer { ConcreteCracked: true }))
+				_stringerCrackLS = (int) CurrentStep;
+			
+			// Check if a panel cracked at the current step
+			if (!_panelCrackLS.HasValue && spmInput.Panels.Any(s => s is NLPanel { ConcreteCracked: true }))
+				_panelCrackLS = (int) CurrentStep;
+		}
+
+		/// <summary>
+		///     Generate an <see cref="SPMOutput" /> from analysis results.
+		/// </summary>
+		public new SPMOutput GenerateOutput() => new (Steps, _stringerCrackLS, _panelCrackLS);
+	}
+}
---
+++ andrefmello91.SPMElements/Analysis/SPMOutput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using andrefmello91.FEMAnalysis;
+
+namespace andrefmello91.SPMElements
+{
+	/// <summary>
+	///		Output class for SPM analysis.
+	/// </summary>
+	public class SPMOutput : FEMOutput
+	{
+		/// <summary>
+		///		Get the number of the load step where the first crack appears at a stringer.
+		/// </summary>
+		public int? StringerCrackLoadStep { get; }
+		
+		/// <summary>
+		///		Get the number of the load step where the first crack appears at a panel.
+		/// </summary>
+		public int? PanelCrackLoadStep { get; }
+		
+		/// <inheritdoc />
+		/// <param name="stringerCrackLoadStep">The number of the load step where the first crack appears at a stringer.</param>
+		/// <param name="panelCrackLoadStep">The number of the load step where the first crack appears at a panel.</param>
+		public SPMOutput(IEnumerable<LoadStep> loadStepResults, int? stringerCrackLoadStep, int? panelCrackLoadStep)
+			: base(loadStepResults)
+		{
+			StringerCrackLoadStep = stringerCrackLoadStep;
+			PanelCrackLoadStep    = panelCrackLoadStep;
+		}
+	}
+}
